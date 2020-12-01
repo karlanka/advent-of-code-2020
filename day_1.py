f8e47fac1b7590b5095910be601b3df1ab1f931f
anton karling <anton.karling@skf.com>
2020-12-01 08:08:45 +0100

    day1

--- day_1.py
+++ day_1.py
@@ -3,21 +3,33 @@ import sys
 
 def one(input):
     """
-    >>> one()
+    >>> one([1721,979,366,299,675,1456])
+    514579
     """
-    return
+    input = sorted(input)
+    for x in input:
+        if 2020 - x in input:
+            return x * (2020-x)
 
 def two(input):
     """
-    >>> two()
+    >>> two([1721,979,366,299,675,1456])
+    241861950
     """
-    return
+    input = sorted(input)
+    for i, x in enumerate(input):
+        goal = 2020 - x
+        for y in input[i:]:
+            if goal - y in input:
+                return (2020 - x - y) * x * y
+
+            
 
 def main():
-    input = get_input(day=0)
-    
+    input = list(map(int, get_input(day=1)))
     print("first:", one(input))
     print("second:", two(input))
+    print(sorted(input))
 
 if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == '-t':
